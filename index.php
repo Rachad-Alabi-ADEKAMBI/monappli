9e599700d9b57818fdb53cb035b1a3a7c0b10aab
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-01-26 21:06:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Mon appli</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+
+        </div>
+
+        <div class="main">
+            <div class="heading">
+                <h1 class="heading__title">
+                    Bienvenue ici
+                </h1>
+            </div>
+
+            <div class="heading__infos">
+                <form action="" method="POST"
+                 class="heading__infos__form">
+
+                 <label for="">
+                     Votre date de naissance: <br>
+                     <input type="date">
+                 </label>
+
+                 <label for="">
+                     <input type="submit">
+                 </label>
+                
+                </form>
+            </div>
+
+            <div class="heading__results">
+                <p class="heading__results__text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Aliquid, ullam. Inventore odio eaque facilis maiores suscipit 
+                    obcaecati recusandae ratione soluta aliquid excepturi,
+                     dolorem illum? Praesentium pariatur voluptas amet impedit provident?
+                </p>
+
+                <div class="heading__results__table">
+
+                </div>
+
+                <a href="">En savoir plus ....</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
